ba4ee07b52a0ed9519acdba54f37b66ae6f76485
Ryan Pratt <ryan.k.pratt@gmail.com>
2023-06-07 22:53:58 -0700

    Second Working Version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Homework-14
+
+## Website
+https://warm-journey-07186.herokuapp.com
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
   
 }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(routes);
 
 
